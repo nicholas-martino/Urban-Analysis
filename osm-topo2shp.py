1c9e361fc0a2e380d52804bf3cfeab0e5832af53
Nicholas Martino <nicholas.martino@hotmail.com>
2019-10-26 21:16:58 -0700

    3d visualization

---
+++ osm-topo2shp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 11:33:24 2019
+
+@author: nicho
+"""
+
+import geocoder
+
+
+
+
+g = geocoder.elevation()
+topo_points = []
+topo_points.append(g)
